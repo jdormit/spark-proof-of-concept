fd4593f40bd0ea59dc9d475961f4ddf0db80b003
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-11-08 09:43:07 -0700

    Enable passing in initial buffer text as string

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -12,11 +12,25 @@
      name: 'editor',
      mounted() {
          const self = this;
-         const writer = new Remington(this.$el, function(inputEvent) {
+         const writer = new Remington(this.$el, this.rows, function(inputEvent) {
              for (let i = 0; i < writer.getBuffer().length; i++) {
                  self.rows.splice(i, 1, writer.getBuffer()[i]);
              }
          });
+     },
+     methods: {
+         parseInitialRows(initialRows) {
+             if (Array.isArray(initialRows)) {
+                 return initialRows;
+             }
+             else {
+                 const rows = initialRows.split('\n');
+                 for (let i = 0; i < rows.length; i++) {
+                     rows[i] += '\n';
+                 }
+                 return rows;
+             }
+         }
      },
      props: {
          'initialRows': {
@@ -25,7 +39,7 @@
          },
          'initialCursors': {
              type: Array,
-             default: () => []
+             default: () => [{row: 0, col: 0}]
          },
          'filename': {
              type: String,
@@ -34,7 +48,7 @@
      },
      data() {
          return {
-             'rows': this.initialRows,
+             'rows': Array.isArray(this.initialRows) ? this.initialRows : this.parseInitialRows(this.initialRows),
              'cursors': this.initialCursors
          };
      }
