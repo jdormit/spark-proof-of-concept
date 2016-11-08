b5e402f2cc1e5e8200796046ffd0ff55f0d01b7f
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-11-08 09:43:40 -0700

    Parse script arguments and read in file text on server start

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+var fs = require('fs')
 var express = require('express');
 var app = express();
+
+var args = process.argv.slice(2);
+if (args.length !== 1) {
+    console.error("Usage: npm start -- filename");
+    process.exit(1);
+}
+var filename = args[0];
+var fileText = fs.readFileSync(filename, {encoding: 'utf8'});
 
 app.use(express.static(__dirname + '/dist'));
 
 app.use('*', function(req, res) {
-    res.sendFile(__dirname + 'dist/index.html');
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(8080);
