29b6a1f008e78f14916d768a6da8f96c271ab3b2
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-11-10 17:41:25 -0700

    Send text file over websocket to client

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var fs = require('fs')
+var fs = require('fs');
+var http = require('http');
+var socketIO = require('socket.io');
 var express = require('express');
+
 var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
 
 var args = process.argv.slice(2);
 if (args.length !== 1) {
@@ -10,10 +15,14 @@ if (args.length !== 1) {
 var filename = args[0];
 var fileText = fs.readFileSync(filename, {encoding: 'utf8'});
 
+io.on('connection', function(socket) {
+    socket.emit('fileText', fileText);
+});
+
 app.use(express.static(__dirname + '/dist'));
 
 app.use('*', function(req, res) {
     res.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(8080);
+server.listen(8080);
