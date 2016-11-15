3d193ba07933855affb92ac74d2543aafafce1fe
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-11-15 09:32:11 -0700

    Store a server-side Remington buffer and receive input from the web socket

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 var fs = require('fs');
 var http = require('http');
 var socketIO = require('socket.io');
 var express = require('express');
+var Remington = require('remington');
 
 var app = express();
 var server = http.Server(app);
@@ -15,8 +17,25 @@ if (args.length !== 1) {
 var filename = args[0];
 var fileText = fs.readFileSync(filename, {encoding: 'utf8'});
 
+var writer = new Remington(null, fileText);
+
 io.on('connection', function(socket) {
-    socket.emit('fileText', fileText);
+    // Another problem with the buffer abstraction:
+    // on the server, the buffer needs to be a string,
+    // not an array of rows
+    socket.emit('fileText', writer.getBufferText());
+
+    socket.on('input', function(inputBuffer) {
+        inputBuffer.forEach(function(input) {
+            writer.setCursor(input.cursor.col, input.cursor.row);
+            writer.sendInput(input.value);
+        });
+    });
+
+    // This probably won't scale
+    socket.on('save', function() {
+        fs.writeFileSync(filename, writer.getBufferText());
+    });
 });
 
 app.use(express.static(__dirname + '/dist'));
